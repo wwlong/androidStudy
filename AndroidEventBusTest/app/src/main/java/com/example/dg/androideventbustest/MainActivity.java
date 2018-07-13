c165c2b8515c8c8c411e66db45e8b0b2a5179cf2
wenlongwang <wenlongwang@deepglint.com>
2018-07-13 16:37:47 +0800

    add EventBus Demo

---
+++ AndroidEventBusTest/app/src/main/java/com/example/dg/androideventbustest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.dg.androideventbustest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button0);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().post(new MessageEvent("Hello everyone!"));
+            }
+        });
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        Toast.makeText(MainActivity.this, event.message, Toast.LENGTH_SHORT).show();
+    }
+
+    // This method will be called when a SomeOtherEvent is posted
+    @Subscribe
+    public void handleSomethingElse(MessageEvent event) {
+        Toast.makeText(MainActivity.this, event.message, Toast.LENGTH_SHORT).show();
+        //doSomethingWith(event);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+
+}
